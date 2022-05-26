428a9ecf7aba40aad289bc65e1bb11b191ba9078
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2022-05-26 05:56:09 -0400

    melhorando

--- js/function.js
+++ js/function.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function () {
 
-  $('.navegacao-mobile').click(function(e){
+  $('.navegacao-mobile').click(function (e) {
     e.stopPropagation();
     $(this).find('ul').slideToggle('fast');
   });
 
-  $('body, html').click(function(e){
+  $('body, html').click(function (e) {
     e.stopPropagation();
     $('.navegacao-mobile').find('ul').slideUp('fast');
   })
